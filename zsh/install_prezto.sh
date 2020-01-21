435f0c53924332d4d29be00bf1cf5f6c1894b7d5
shino-kei <shino_kei@asagi.waseda.jp>
2020-01-21 17:39:00 +0900

    add install script for prezto

---
+++ zsh/install_prezto.sh
@@ -0,0 +1,14 @@
+
+if [ ! -e ~/.zprezto ]; then 
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  mkdir zsh_orig && mv zshmv .zlogin .zlogout .zprofile .zshenv .zshrc zsh_orig
+
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+else
+  echo "prezto already installed"
+
+fi
+
