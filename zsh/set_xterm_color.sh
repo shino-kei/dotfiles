83f851f59c11edcfe1cd71abf6b4a1acf2f53f94
shino-kei <shino_kei@asagi.waseda.jp>
2020-08-27 13:23:11 +0900

    handle signal

--- zsh/set_xterm_color.sh
+++ zsh/set_xterm_color.sh
@@ -3,9 +3,19 @@ if type "xtermcontrol" > /dev/null 2>&1; then
   THEME_DIR=~/.theme
   DEFAULT_THEME=google_dark.theme
   SSH_THEME=solarized_dark.theme
-  ssh() {
-    xtermcontrol --file=${THEME_DIR}/${SSH_THEME}
-    command ssh "$@"
+
+  function set_default_theme() {
     xtermcontrol --file=${THEME_DIR}/${DEFAULT_THEME}
+  }
+  
+  function set_ssh_theme() {
+    xtermcontrol --file=${THEME_DIR}/${SSH_THEME}
+  }
+
+  ssh() {
+    trap set_default_theme 1 2 3 15
+    set_ssh_theme
+    command ssh "$@"
+    set_default_theme
   }
 fi
