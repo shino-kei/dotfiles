e9d5a4d750608dbd0df25a4f0c61f0410c93b1f5
shino-kei <shino_kei@asagi.waseda.jp>
2020-12-11 16:25:18 +0900

    add path settings for golang

--- golang/golang.sh
+++ golang/golang.sh
@@ -1,5 +1,13 @@
-# for go lang
 if [ -x "`which go`" ]; then
-  export GOPATH=$HOME/go
+  export GOPATH=$HOME/3rdparty/go
   export PATH="$GOPATH/bin:$PATH"
+
+  
+  if [ ! -x $GOPATH ]; then
+    echo "GOPATH does not exit. create it? (y/N):"  
+    read -n 1 yn
+    if [[ $yn = [yY] ]]; then
+      mkdir -p ${GOPATH}
+    fi
+  fi
 fi
