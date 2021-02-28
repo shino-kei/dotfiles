e4ad2e4ba14f882aace74157ce5bb192b2fafc28
shino-kei <shino_kei@asagi.waseda.jp>
2021-02-28 11:19:54 +0900

    fix yes/no prompt

--- golang/golang.sh
+++ golang/golang.sh
@@ -2,12 +2,13 @@ if [ -x "`which go`" ]; then
   export GOPATH=$HOME/3rdparty/go
   export PATH="$GOPATH/bin:$PATH"
 
-  
   if [ ! -x $GOPATH ]; then
-    echo "GOPATH does not exit. create it? (y/N):"  
-    read -n 1 yn
-    if [[ $yn = [yY] ]]; then
+    echo "GOPATH(${GOPATH})does not exit. create it? (y/N):"  
+    if read -q; then
+      echo "create ${GOPATH}"
       mkdir -p ${GOPATH}
+    else
+      echo abort
     fi
   fi
 fi
